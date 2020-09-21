8057915e23bf4a33d45a8a09c5511a57f066fecf
iliess-np <iliessnapolion@gmail.com>
2020-09-21 17:04:43 +0100

    1

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-Array.from(document.querySelectorAll(".contact")).forEach((e) => {
+Array.from(
+  document.querySelectorAll(".bay_div_coffee , .bay_div_tea ")
+).forEach((e) => {
   const imgs = Array.from(e.querySelectorAll("img"));
   new hoverEffect({
     parent: e,
     intensity: 0.3,
     image1: imgs[0].getAttribute("src"),
     image2: imgs[1].getAttribute("src"),
-    displacementImage: "./images/displacement/6.jpg",
+    displacementImage: "./images/displacement/1.jpg",
   });
 });
